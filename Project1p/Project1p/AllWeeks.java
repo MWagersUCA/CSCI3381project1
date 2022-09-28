52f6bfdeb87e323c719d1fc89caf8f9860250f5c
MWagersUCA <98266137+MWagersUCA@users.noreply.github.com>
2022-09-28 11:53:47 -0500

    fixed File reader

---
+++ Project1p/Project1p/AllWeeks.java
@@ -0,0 +1,184 @@
+package Project1p;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class AllWeeks {
+	private ArrayList<ShowWeek> shows;
+	private ArrayList<String> purgedShows;
+	private String listName;
+	private String fileName;
+	
+	
+	public AllWeeks() {
+		shows = new ArrayList<ShowWeek>();
+		purgedShows = new ArrayList<String>();
+		listName = "a name";
+		fileName = null;
+	}
+	
+	public AllWeeks(String ln, String fn) {
+		this();
+		listName = ln;
+		fileName = fn;
+		readFile();
+	}
+	
+	public void addShow(ShowWeek s) {
+		shows.add(s);
+	}
+
+	public String toString() {
+		String toReturn = listName + ":\n";
+		for(ShowWeek s: shows) {
+			toReturn += s.toString() + "\n";
+		}
+		return toReturn;
+	}
+	
+	public void purge(String s) {
+		purgedShows.add(s);
+	}
+	public void unPurge(String s) {
+		if(purgedShows.contains(s)) {
+			purgedShows.remove(s);
+		}
+		else {
+			System.out.println("Unable to locate:  " + s);
+		}
+	}
+	public boolean isPurged(String s) {
+		if(purgedShows.contains(s)) {
+			return true;
+		}
+		return false;
+	}
+	
+	public String ranSuggest() {
+		int index = (int) (Math.random() * shows.size());
+		return shows.get(index).getShow_titles();
+	}
+	public String predictiveSuggest(String s) {
+		String toReturn = ranSuggest();
+		while(toReturn == s || isPurged(toReturn)) {
+			toReturn = ranSuggest();
+		}
+		return toReturn;
+	}
+	
+	public String getShowsinWeek(String s) {
+		String toReturn = "Shows in selected week:\n";
+		for(ShowWeek sw : shows) {
+			if(sw.getWeek().equals(s)) {
+				toReturn += sw.getShow_titles() + "\n";
+			}
+		}
+		return toReturn;
+	}
+	
+	private void readFile () {
+		BufferedReader lineReader = null;
+		try {
+			FileReader fr = new FileReader(fileName);
+			lineReader = new BufferedReader(fr);
+			String week = null;
+			while ((week = lineReader.readLine())!=null) {
+				String category = lineReader.readLine();
+				String weekly_rank = lineReader.readLine();
+				String show_titles = lineReader.readLine();
+				String season_title = lineReader.readLine();
+				String weekly_hours_viewed = lineReader.readLine();
+				String cumulative_weeks_in_top10 = lineReader.readLine();
+				addShow(new ShowWeek(week, category, weekly_rank, show_titles, season_title, weekly_hours_viewed, cumulative_weeks_in_top10));
+					
+				}
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.err.println("there was a problem with the file reader, try different read type.");
+			try {
+				lineReader = new BufferedReader(new InputStreamReader(this.getClass().getResourceAsStream(fileName.substring(1))));
+				String week = null;
+				while ((week = lineReader.readLine())!=null) {
+					String category = lineReader.readLine();
+					String weekly_rank = lineReader.readLine();
+					String show_titles = lineReader.readLine();
+					String season_title = lineReader.readLine();
+					String weekly_hours_viewed = lineReader.readLine();
+					String cumulative_weeks_in_top10 = lineReader.readLine();
+					//addShow(new ShowWeek(week, category, weekly_rank, show_titles, season_title, weekly_hours_viewed, cumulative_weeks_in_top10));
+			
+					}
+			} catch (Exception e2) {
+				System.err.println("there was a problem with the file reader, try again.  either no such file or format error");
+			} finally {
+				if (lineReader != null)
+					try {
+						lineReader.close();
+					} catch (IOException e2) {
+						System.err.println("could not close BufferedReader");
+					}
+			}			
+		} finally {
+			if (lineReader != null)
+				try {
+					lineReader.close();
+				} catch (IOException e) {
+					System.err.println("could not close BufferedReader");
+				}
+		}
+	} // end of readFile method	
+	
+	public void writeFile () {
+		// overloaded method: this calls doWrite with file used to read data
+		// use this for saving data between runs
+		doWrite(fileName);
+	} // end of writeFile method
+
+	public void writeFile(String altFileName) {
+		// overloaded method: this calls doWrite with different file name 
+		// use this for testing write
+		doWrite(altFileName);		
+	}// end of writeFile method
+	
+	private void doWrite(String fn) {
+		// this method writes all of the data in the persons array to a file
+		try
+		{
+
+			FileWriter fw = new FileWriter(fn);
+			BufferedWriter myOutfile = new BufferedWriter(fw);			
+			
+			for (int i = 0; i < size; i++) {
+				Person person = people[i];
+				if (person instanceof Student) {
+					myOutfile.write ("student\n");
+					myOutfile.write (person.getName()+"\n");
+					myOutfile.write (person.getId()+"\n");
+					myOutfile.write (((Student) person).getGpa()+"\n");
+				}
+				else if (person instanceof Instructor) {
+					myOutfile.write ("instructor\n");
+					myOutfile.write (person.getName()+"\n");
+					myOutfile.write (person.getId()+"\n");
+					myOutfile.write (((Instructor) person).getEmail()+"\n");
+				}	
+				else {
+					System.err.println("error in array, instance type not found");
+				}
+			}
+			myOutfile.flush();
+			myOutfile.close();
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			System.err.println("Didn't save to " + fn);
+		}
+	}	
+
+}
--- Project1p/Project1p/project1main.java
+++ Project1p/Project1p/project1main.java
@@ -3,8 +3,45 @@ package Project1p;
 public class project1main {
 
 	public static void main(String[] args) {
+		ShowWeek sw1 = new ShowWeek();
+		AllWeeks myList = new AllWeeks();
+		
+		System.out.println(sw1);
+		ShowWeek sw2 = new ShowWeek("9-12-20", "Drama", "4", "Breaking Bad", "Season 1", "72000000", "10");
+		System.out.println(sw2);
+		System.out.println(sw1.equals(sw2)); //expect false
+		System.out.println(sw2.equals(sw2)); //expect true
+		
+		myList.addShow(sw2);
+		ShowWeek sw3 = new ShowWeek("9-12-20", "Romance", "2", "Outlander", "Season 3", "4523000", "2");
+		ShowWeek sw4 = new ShowWeek("9-12-20", "Drama", "1", "Do Revenge", "N/A", "42550000", "2");
+		ShowWeek sw5 = new ShowWeek("9-19-20", "Action", "3", "Lou", "Season 4", "40570000", "1");
+		myList.addShow(sw3);
+		myList.addShow(sw4);
+		myList.addShow(sw5);
+		System.out.println(myList.ranSuggest());
+		System.out.println(myList.predictiveSuggest("Outlander")); //Suggestion should not contain Outlander
+		myList.purge("Breaking Bad");
+		System.out.println(myList.predictiveSuggest("Outlander")); //Suggestion should not contain Outlander || Breaking Bad
+		myList.unPurge("Breaking Bad");
+		System.out.println(myList.predictiveSuggest("Outlander")); //Suggestion should not contain Outlander
+		
+		String myWeekList = myList.getShowsinWeek("9-12-20");
+		System.out.println(myWeekList);
+		
+		AllWeeks allData = new AllWeeks("AllShows", "netflixAllWeeksGlobalProcessed.txt");
+		myWeekList = allData.getShowsinWeek("2021-09-26");
+		System.out.println(myWeekList);
+		allData.purge("The Circle");
+		System.out.println(allData.predictiveSuggest("Good Girls"));
+		//System.out.println(allData);
+		//allData.addShow(sw1);
+		//allData.addShow(sw2);
+		//System.out.println(allData);
+		//System.out.println(allData.ranSuggest());
 		
 
+		
 	}
 
 }
